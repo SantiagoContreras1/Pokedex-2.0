b32faf3b78f0f3aac2af013556b672fba0ee2a13
SantiagoContreras1 <168132510+SantiagoContreras1@users.noreply.github.com>
2024-08-31 01:17:02 -0600

    Este es el legible

---
+++ src/components/BuscarPoke.jsx
@@ -0,0 +1,7 @@
+import {useState} from 'react'
+
+export const BuscarPoke = ({handleGetPoke}) => {
+  return (
+    <div>BuscarPoke</div>
+  )
+}
---
+++ src/service/poke.jsx
@@ -0,0 +1,11 @@
+/* import React from 'react'
+
+
+export const reqPoke = async() => {
+    try {
+        
+
+    } catch (err) {
+        console.log(err)
+    }
+} */
